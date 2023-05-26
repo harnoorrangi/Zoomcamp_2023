fe997c8f55ff894ee0e6650b86a98742e3c33799
Harnoor <harnoorrangi@hotmail.com>
2023-05-26 17:48:03 -0400

    Assignment2 Code added

--- cohorts/2023/02-experiment-tracking/homework/train.py
+++ cohorts/2023/02-experiment-tracking/homework/train.py
@@ -2,6 +2,11 @@ import os
 import pickle
 import click
 
+import mlflow
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Assignment_2")
+mlflow.sklearn.autolog()
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
@@ -22,11 +27,13 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
