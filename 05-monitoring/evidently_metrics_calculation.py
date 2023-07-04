f5970c9113bc6412c83d12995e28fd9dffd9adf0
Harnoor <harnoorrangi@hotmail.com>
2023-07-03 21:09:46 -0400

    Assignment 5

--- 05-monitoring/evidently_metrics_calculation.py
+++ 05-monitoring/evidently_metrics_calculation.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 import random
-import logging 
+import logging
 import uuid
 import pytz
 import pandas as pd
@@ -13,9 +13,10 @@ from prefect import task, flow
 
 from evidently.report import Report
 from evidently import ColumnMapping
-from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric
+from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric, ColumnQuantileMetric, DatasetSummaryMetric
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s [%(levelname)s]: %(message)s")
 
 SEND_TIMEOUT = 10
 rand = random.Random()
@@ -26,18 +27,20 @@ create table dummy_metrics(
 	timestamp timestamp,
 	prediction_drift float,
 	num_drifted_columns integer,
-	share_missing_values float
+	share_missing_values float,
+	fare_quantile float
 )
 """
 
 reference_data = pd.read_parquet('data/reference.parquet')
 with open('models/lin_reg.bin', 'rb') as f_in:
-	model = joblib.load(f_in)
+    model = joblib.load(f_in)
 
-raw_data = pd.read_parquet('data/green_tripdata_2022-02.parquet')
+raw_data = pd.read_parquet('data/green_tripdata_2023-03.parquet')
 
-begin = datetime.datetime(2022, 2, 1, 0, 0)
-num_features = ['passenger_count', 'trip_distance', 'fare_amount', 'total_amount']
+begin = datetime.datetime(2023, 3, 1, 0, 0)
+num_features = ['passenger_count',
+                'trip_distance', 'fare_amount', 'total_amount']
 cat_features = ['PULocationID', 'DOLocationID']
 column_mapping = ColumnMapping(
     prediction='prediction',
@@ -46,59 +49,68 @@ column_mapping = ColumnMapping(
     target=None
 )
 
-report = Report(metrics = [
+report = Report(metrics=[
     ColumnDriftMetric(column_name='prediction'),
     DatasetDriftMetric(),
-    DatasetMissingValuesMetric()
+    DatasetMissingValuesMetric(),
+    ColumnQuantileMetric(column_name="fare_amount", quantile=0.5),
+    DatasetSummaryMetric()
 ])
 
-@task
+
+@task(name="prepare db", retries=2, retry_delay_seconds=5)
 def prep_db():
-	with psycopg.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
-		res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
-		if len(res.fetchall()) == 0:
-			conn.execute("create database test;")
-		with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example") as conn:
-			conn.execute(create_table_statement)
+    with psycopg.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
+        res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
+        if len(res.fetchall()) == 0:
+            conn.execute("create database test;")
+        with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example") as conn:
+            conn.execute(create_table_statement)
 
-@task
+
+@task(name="prepare metrics", retries=2, retry_delay_seconds=5)
 def calculate_metrics_postgresql(curr, i):
-	current_data = raw_data[(raw_data.lpep_pickup_datetime >= (begin + datetime.timedelta(i))) &
-		(raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]
+    current_data = raw_data[(raw_data.lpep_pickup_datetime >= (begin + datetime.timedelta(i))) &
+                            (raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]
 
-	#current_data.fillna(0, inplace=True)
-	current_data['prediction'] = model.predict(current_data[num_features + cat_features].fillna(0))
+    # current_data.fillna(0, inplace=True)
+    current_data['prediction'] = model.predict(
+        current_data[num_features + cat_features].fillna(0))
 
-	report.run(reference_data = reference_data, current_data = current_data,
-		column_mapping=column_mapping)
+    report.run(reference_data=reference_data,
+               current_data=current_data, column_mapping=column_mapping)
 
-	result = report.as_dict()
+    result = report.as_dict()
 
-	prediction_drift = result['metrics'][0]['result']['drift_score']
-	num_drifted_columns = result['metrics'][1]['result']['number_of_drifted_columns']
-	share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
+    prediction_drift = result['metrics'][0]['result']['drift_score']
+    num_drifted_columns = result['metrics'][1]['result']['number_of_drifted_columns']
+    share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
+    fare_quantile = result['metrics'][3]['result']['current']['value']
 
-	curr.execute(
-		"insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
-		(begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values)
-	)
+    curr.execute(
+        "insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values, fare_quantile) values (%s, %s, %s, %s, %s)",
+        (begin + datetime.timedelta(i), prediction_drift,
+         num_drifted_columns, share_missing_values, fare_quantile)
+    )
+
 
 @flow
 def batch_monitoring_backfill():
-	prep_db()
-	last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
-	with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example", autocommit=True) as conn:
-		for i in range(0, 27):
-			with conn.cursor() as curr:
-				calculate_metrics_postgresql(curr, i)
+    prep_db()
+    last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
+    with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example", autocommit=True) as conn:
+        for i in range(0, 27):
+            with conn.cursor() as curr:
+                calculate_metrics_postgresql(curr, i)
 
-			new_send = datetime.datetime.now()
-			seconds_elapsed = (new_send - last_send).total_seconds()
-			if seconds_elapsed < SEND_TIMEOUT:
-				time.sleep(SEND_TIMEOUT - seconds_elapsed)
-			while last_send < new_send:
-				last_send = last_send + datetime.timedelta(seconds=10)
-			logging.info("data sent")
+            new_send = datetime.datetime.now()
+            seconds_elapsed = (new_send - last_send).total_seconds()
+            if seconds_elapsed < SEND_TIMEOUT:
+                time.sleep(SEND_TIMEOUT - seconds_elapsed)
+            while last_send < new_send:
+                last_send = last_send + datetime.timedelta(seconds=10)
+            logging.info("data sent")
+
 
 if __name__ == '__main__':
-	batch_monitoring_backfill()
+    batch_monitoring_backfill()
